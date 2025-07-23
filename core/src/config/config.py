134a69207d87b3c842d6d2f09c266273f063e91b
Jai Aggarwal <jaiaggarwal14032002@gmail.com>
2025-07-23 13:00:36 +0530

    adding the github secret

--- core/src/config/config.py
+++ core/src/config/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     MONGO_URI: str
     DB_NAME: str = "bernerspace"
     GCP_BUCKET: str
-    GOOGLE_APPLICATION_CREDENTIALS: str # This will hold the GCS path
+    GCP_SECRET_NAME: str # This will hold the name of the Secret Manager secret
     CLIENT_ID: str
     CLIENT_SECRET: str 
 
